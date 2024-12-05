4b623892d637c850d02d325da3cb1346eee32ce9
notKamui <teillard.jimmy@outlook.fr>
2024-12-05 15:24:29 +0100

    Refactor input handling and improve safety checks in day 1 and day 2 runs

--- src/day1/run.ts
+++ src/day1/run.ts
@@ -1,17 +1,18 @@
-const file = Bun.file('./src/day1/input.txt')
+const input = require('./input.txt').default as string
 
 export async function run() {
-  const input = await file.text()
-
   const [left, right] = input
     .split('\n')
     .filter(Boolean)
-    .reduce(([l, r], line) => {
-      const [left, right] = line.replace(/ {2}/g, '').split(' ')
-      l.push(Number.parseInt(left))
-      r.push(Number.parseInt(right))
-      return [l, r]
-    }, [[] as number[], [] as number[]])
+    .reduce(
+      ([l, r], line) => {
+        const [left, right] = line.replace(/ {2}/g, '').split(' ')
+        l.push(Number.parseInt(left))
+        r.push(Number.parseInt(right))
+        return [l, r]
+      },
+      [[] as number[], [] as number[]],
+    )
 
   part1(left, right)
   part2(left, right)
@@ -31,12 +32,18 @@ function part1(left: number[], right: number[]) {
 }
 
 function part2(left: number[], right: number[]) {
-  const appearances = right.reduce((acc, id) => {
-    acc[id] = (acc[id] || 0) + 1
-    return acc
-  }, {} as Record<number, number>)
+  const appearances = right.reduce(
+    (acc, id) => {
+      acc[id] = (acc[id] || 0) + 1
+      return acc
+    },
+    {} as Record<number, number>,
+  )
 
-  const similarity = left.reduce((acc, id) => acc + (appearances[id] || 0) * id, 0)
+  const similarity = left.reduce(
+    (acc, id) => acc + (appearances[id] || 0) * id,
+    0,
+  )
 
   console.log('Similarity: ', similarity)
 }
--- src/day2/run.ts
+++ src/day2/run.ts
@@ -1,9 +1,8 @@
-import { zipWithNext } from '../utils/zip'
+import { zipWithNext } from "../utils/zip";
 
-const file = Bun.file('./src/day2/input.txt')
+const input = require('./input.txt').default as string
 
 export async function run() {
-  const input = await file.text()
   const reports = input
     .split('\n')
     .filter(Boolean)
@@ -14,34 +13,33 @@ export async function run() {
 }
 
 function part1(reports: number[][]) {
-  function isSafe(report: number[]) {
-    // either all increasing or all decreasing, and each level has a distance between 1 and 3 included
-    const zipped = zipWithNext(report)
-    const diffs = zipped.map(([a, b]) => a - b)
-    if (diffs.some((d) => Math.abs(d) < 1 || Math.abs(d) > 3)) return false
-    if (diffs.some((d) => d < 0) && diffs.some((d) => d > 0)) return false
-    return true
-  }
-
   const safe = reports.filter(isSafe)
 
   console.log('Safe reports: ', safe.length)
 }
 
 function part2(reports: number[][]) {
-  function isSafe(report: number[]) {
-    // same as before except one error is tolerated
-    const zipped = zipWithNext(report)
-    const diffs = zipped.map(([a, b]) => a - b)
-    const diffErrors = diffs.filter((d) => Math.abs(d) < 1 || Math.abs(d) > 3).length
-    if (diffErrors > 1) return false
-    const countIncreasing = diffs.filter((d) => d < 0).length
-    const diff = zipped.length - countIncreasing
-    if (diff > 1 && diff < zipped.length - 1) return false
-    return true
-  }
-
-  const safe = reports.filter(isSafe)
+  const safe = reports.filter(isSafeWithDampener)
 
   console.log('Safe reports: ', safe.length)
+}
+
+function isSafe(report: number[]) {
+  // either all increasing or all decreasing, and each level has a distance between 1 and 3 included
+  const zipped = zipWithNext(report)
+  const diffs = zipped.map(([a, b]) => a - b)
+  if (diffs.some((d) => Math.abs(d) < 1 || Math.abs(d) > 3)) return false
+  if (diffs.some((d) => d < 0) && diffs.some((d) => d > 0)) return false
+  return true
+}
+
+function isSafeWithDampener(report: number[]) {
+  // same as before except that if at most one level can be skipped to make it safe, it is safe
+  if (isSafe(report)) return true
+  for (let i = 0; i < report.length; i++) {
+    const without = report.slice()
+    without.splice(i, 1)
+    if (isSafe(without)) return true
+  }
+  return false
 }
